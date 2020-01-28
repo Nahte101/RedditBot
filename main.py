d98654f2130f0e7bbdb5f1302ff7338f54f327f8
Ethan <smith.ethan50@gmail.com>
2020-01-28 19:33:17 +1100

    Added Text Posting

--- main.py
+++ main.py
@@ -16,13 +16,19 @@ title = input("What shall the title of the post be? ")
 good_input = False
 
 while not good_input:
-    post_type = input("Image or Video? (type img or vid) ")
+    post_type = input("Image/Video/Text Post? (type img or vid or txt) ")
     if post_type == 'img':
         good_input = True
     elif post_type == 'vid':
         good_input = True
+    elif post_type == 'txt':
+        good_input = True
 
-directory = input("Directory to file: ")
+
+if post_type == 'txt':
+    nonTitleText = input("What will the text be below title? ")
+else:
+    directory = input("Directory to file: ")
 time_to_post = input("Time to be posted (AU Time) mm:dd:hh:mm ")
 not_posted = True
 
@@ -36,17 +42,21 @@ def checkTimeArraysEqual(arr1,arr2):#must be of same length
             break
     return match
 
-print("Processing")
+print("Waiting")
+
 while not_posted:
     currenttime = time.localtime(time.time())
     currentdatetime = currenttime[1:5]
     if checkTimeArraysEqual(date_to_post,currentdatetime):
         if post_type == 'img':
-            subreddit.submit_image(title=title, image_path=directory)
+            subreddit.submit_image(title=title, image_path=directory,without_websockets=True)
             not_posted = False
             print('posted')
         elif post_type == 'vid':
-            subreddit.submit_video(title=title, video_path=directory)
+            subreddit.submit_video(title=title, video_path=directory,without_websockets=True)
             not_posted = False
             print('posted')
-
+        elif post_type == 'txt':
+            subreddit.submit(title=title,selftext=nonTitleText)
+            not_posted = False
+            print('posted')
